1a23af7b371b9242539e0fb66ba7391123758552
Edmond Qiu (RHA) <Edmond.Qiu@rexelha.com.au>
2019-02-05 11:33:09 +1100

    lol1

---
+++ web_resources/cool.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+$(document).ready(function () {
+    $('.testbutton').click(function (event){
+        var name=$('.name').val()
+        var grade=$('.grade').val()
+        
+    })
+})
+
